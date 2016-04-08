f336fdeb9178ec56ba74a04a2e7c3231578da84c
yassin <youfkir@icloud.com>
2016-04-08 13:19:49 +0200

    Login added & upload to raptocloud.net / 14350.hosts.ma-cloud.nl  Changes to be committed: 	new file:   check.php 	new file:   contact.php 	new file:   index.php 	new file:   login.php 	new file:   store.php

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+// standaard wachtwoord en naam omdat we geen database gebruiken
+$user = 'ayy';
+$pass = 'lmao';
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    
+    if (($_POST['username'] == $user) && ($_POST['password'] == $pass)) {    
+        
+        if (isset($_POST['rememberme'])) {
+            // cookie bechimmeld na 1 jaar
+            setcookie('username', $_POST['username'], time()+60*60*24*365, '/account', 'https://raptorcloud.net/acme');
+            setcookie('password', md5($_POST['password']), time()+60*60*24*365, '/account', 'https://raptorcloud.net/acme');
+        
+        } else {
+            // zapt de cookie weg als de client zijn browser sluit (soort van....)
+            setcookie('username', $_POST['username'], false, '/account', 'https://raptorcloud.net/acme');
+            setcookie('password', md5($_POST['password']), false, '/account', 'https://raptorcloud.net/acme');
+        }
+        header('Location: index.php');
+        // error handling (moet nog veranderd worden)
+    } else {
+        echo 'Username/Password Invalid';
+    }
+    
+} else {
+    echo 'You must supply a username and password.';
+}
+?>
