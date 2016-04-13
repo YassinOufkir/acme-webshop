cf49228b6de303d4291a155e6b6f2c5a7ed218d6
yassin <youfkir@icloud.com>
2016-04-13 11:27:35 +0200

    fixed store once again
    shopping cart needs some work from Front end developers

---
+++ landing.php
@@ -0,0 +1,12 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>HQA - High Qualtiy Arms</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>Welcome to HQA</h1>
+</body>
+</html>
